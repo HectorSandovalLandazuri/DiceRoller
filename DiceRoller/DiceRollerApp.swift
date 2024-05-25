81f7a5d0a170bac41e2c1ce8e6fd54b15105ad53
Héctor Sandoval <ingsan@live.com.mx>
2024-05-25 12:04:23 -0600

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
